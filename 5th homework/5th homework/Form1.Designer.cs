2d67af9ee8bcb14045d443e3fc088b8e4b7101d6
hothaifa zoubi <45634350+hothaifa96@users.noreply.github.com>
2018-12-26 12:54:46 +0200

    Add files via upload

---
+++ 5th homework/5th homework/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace _5th_homework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plusBt = new System.Windows.Forms.Button();
+            this.minBt = new System.Windows.Forms.Button();
+            this.timesBt = new System.Windows.Forms.Button();
+            this.divideBt = new System.Windows.Forms.Button();
+            this.powerBt = new System.Windows.Forms.Button();
+            this.rootBt = new System.Windows.Forms.Button();
+            this.firstTXTB = new System.Windows.Forms.TextBox();
+            this.secondTXTB = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.clearBt = new System.Windows.Forms.Button();
+            this.randomBt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // plusBt
+            // 
+            this.plusBt.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            this.plusBt.Location = new System.Drawing.Point(193, 24);
+            this.plusBt.Name = "plusBt";
+            this.plusBt.Size = new System.Drawing.Size(75, 23);
+            this.plusBt.TabIndex = 0;
+            this.plusBt.Text = "+";
+            this.plusBt.UseVisualStyleBackColor = true;
+            this.plusBt.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // minBt
+            // 
+            this.minBt.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.minBt.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minBt.Location = new System.Drawing.Point(193, 53);
+            this.minBt.Name = "minBt";
+            this.minBt.Size = new System.Drawing.Size(75, 23);
+            this.minBt.TabIndex = 1;
+            this.minBt.Text = "-";
+            this.minBt.UseVisualStyleBackColor = true;
+            this.minBt.Click += new System.EventHandler(this.minBt_Click);
+            // 
+            // timesBt
+            // 
+            this.timesBt.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timesBt.Location = new System.Drawing.Point(193, 82);
+            this.timesBt.Name = "timesBt";
+            this.timesBt.Size = new System.Drawing.Size(75, 23);
+            this.timesBt.TabIndex = 2;
+            this.timesBt.Text = "*";
+            this.timesBt.UseVisualStyleBackColor = true;
+            this.timesBt.Click += new System.EventHandler(this.timesBt_Click);
+            // 
+            // divideBt
+            // 
+            this.divideBt.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.divideBt.Location = new System.Drawing.Point(193, 111);
+            this.divideBt.Name = "divideBt";
+            this.divideBt.Size = new System.Drawing.Size(75, 23);
+            this.divideBt.TabIndex = 3;
+            this.divideBt.Text = "/";
+            this.divideBt.UseVisualStyleBackColor = true;
+            this.divideBt.Click += new System.EventHandler(this.divideBt_Click);
+            // 
+            // powerBt
+            // 
+            this.powerBt.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.powerBt.Location = new System.Drawing.Point(193, 140);
+            this.powerBt.Name = "powerBt";
+            this.powerBt.Size = new System.Drawing.Size(75, 23);
+            this.powerBt.TabIndex = 4;
+            this.powerBt.Text = "^";
+            this.powerBt.UseVisualStyleBackColor = true;
+            this.powerBt.Click += new System.EventHandler(this.powerBt_Click);
+            // 
+            // rootBt
+            // 
+            this.rootBt.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rootBt.Location = new System.Drawing.Point(193, 169);
+            this.rootBt.Name = "rootBt";
+            this.rootBt.Size = new System.Drawing.Size(75, 23);
+            this.rootBt.TabIndex = 5;
+            this.rootBt.Text = "Root";
+            this.rootBt.UseVisualStyleBackColor = true;
+            this.rootBt.Click += new System.EventHandler(this.rootBt_Click);
+            // 
+            // firstTXTB
+            // 
+            this.firstTXTB.Location = new System.Drawing.Point(12, 102);
+            this.firstTXTB.Name = "firstTXTB";
+            this.firstTXTB.Size = new System.Drawing.Size(172, 20);
+            this.firstTXTB.TabIndex = 6;
+            // 
+            // secondTXTB
+            // 
+            this.secondTXTB.Location = new System.Drawing.Point(274, 102);
+            this.secondTXTB.Name = "secondTXTB";
+            this.secondTXTB.Size = new System.Drawing.Size(172, 20);
+            this.secondTXTB.TabIndex = 7;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(477, 82);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(311, 238);
+            this.listBox1.TabIndex = 8;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // clearBt
+            // 
+            this.clearBt.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearBt.Location = new System.Drawing.Point(510, 349);
+            this.clearBt.Name = "clearBt";
+            this.clearBt.Size = new System.Drawing.Size(213, 36);
+            this.clearBt.TabIndex = 9;
+            this.clearBt.Text = "clear!";
+            this.clearBt.UseVisualStyleBackColor = true;
+            this.clearBt.Click += new System.EventHandler(this.clearBt_Click);
+            // 
+            // randomBt
+            // 
+            this.randomBt.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.randomBt.Location = new System.Drawing.Point(12, 266);
+            this.randomBt.Name = "randomBt";
+            this.randomBt.Size = new System.Drawing.Size(434, 23);
+            this.randomBt.TabIndex = 10;
+            this.randomBt.Text = "Fill With Random";
+            this.randomBt.UseVisualStyleBackColor = true;
+            this.randomBt.Click += new System.EventHandler(this.randomBt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.randomBt);
+            this.Controls.Add(this.clearBt);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.secondTXTB);
+            this.Controls.Add(this.firstTXTB);
+            this.Controls.Add(this.rootBt);
+            this.Controls.Add(this.powerBt);
+            this.Controls.Add(this.divideBt);
+            this.Controls.Add(this.timesBt);
+            this.Controls.Add(this.minBt);
+            this.Controls.Add(this.plusBt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button plusBt;
+        private System.Windows.Forms.Button minBt;
+        private System.Windows.Forms.Button timesBt;
+        private System.Windows.Forms.Button divideBt;
+        private System.Windows.Forms.Button powerBt;
+        private System.Windows.Forms.Button rootBt;
+        private System.Windows.Forms.TextBox firstTXTB;
+        private System.Windows.Forms.TextBox secondTXTB;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button clearBt;
+        private System.Windows.Forms.Button randomBt;
+    }
+}
+
---
+++ 5th homework/5th homework/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _5th_homework
+{
+    public partial class Form1 : Form
+    {
+        private object debug;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x, y;
+            ConvertTextBoxValue(out x, out y);
+            listBox1.Items.Add($"{x} + {y} = {x+y}");
+        }
+
+        private void ConvertTextBoxValue(out double x, out double y)
+        {
+            
+             double.TryParse(firstTXTB.Text,out x);
+             double.TryParse(secondTXTB.Text,out y );
+            
+               
+
+        }
+
+        private void minBt_Click(object sender, EventArgs e)
+        {
+            double x, y;
+            ConvertTextBoxValue(out x, out y);
+            listBox1.Items.Add($"{x} - {y} = {x - y}");
+        }
+
+        private void timesBt_Click(object sender, EventArgs e)
+        {
+            double x, y;
+            ConvertTextBoxValue(out x, out y);
+            listBox1.Items.Add($"{x} * {y} = {x * y}");
+        }
+
+        private void divideBt_Click(object sender, EventArgs e)
+        {
+            double x, y;
+            ConvertTextBoxValue(out x, out y);
+            if (y != 0)
+                listBox1.Items.Add($"{x} / {y} = {x / y}");
+            else
+            {
+                listBox1.Items.Add("you can't divide by 0 try again");
+                this.BackColor = System.Drawing.Color.Red;
+            }
+        }
+
+        private void powerBt_Click(object sender, EventArgs e)
+        {
+            double x, y;
+            ConvertTextBoxValue(out x, out y);
+            listBox1.Items.Add(y!=0? $"{x} ^ {y} = {Math.Pow(x,y)}": $"{x} ^ {y} = 1");
+        }
+
+        private void rootBt_Click(object sender, EventArgs e)
+        {
+            double x, y;
+            ConvertTextBoxValue(out x, out y);
+            listBox1.Items.Add($"{x} root {y} = {Math.Pow(x,( 1.0/y))}");
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clearBt_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Debug.WriteLine("listbox cleard !!!");
+        }
+
+        private void randomBt_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            firstTXTB.Clear();secondTXTB.Clear();
+            firstTXTB.Text = $"{r.Next(0,101)}";
+            secondTXTB.Text = $"{r.Next(0, 101)}";
+
+        }
+    }
+}
